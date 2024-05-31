65f9bfed7bf6a94207b3f69d606d6072403a4d65
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-31 12:58:57 +0300

    Add tests for books list view

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from books.models import Author, Book
 
-# Create your tests here.
+class Tests(TestCase):
+    def setUp(self):
+        self.authors = [Author.objects.create(name="Author 1", bio=""),Author.objects.create(name="Author 2", bio="")]
+        self.book = Book.objects.create(title="Book 1", description="", publication_date="2024-01-01", cover_image=SimpleUploadedFile(name='1.png', content=''), price=100)
+        self.book.authors.set(self.authors)
+
+    def test_books_list_view_returns_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'book_list.html')
+
+    def test_books_list_view_returns_books(self):
+        response = self.client.get('/')
+        self.assertContains(response, self.book.title)
