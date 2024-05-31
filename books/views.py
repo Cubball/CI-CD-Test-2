579538a340154f624946bdf9dd159586ce70093c
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-31 12:56:17 +0300

    Add view for book list

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from books.models import Author, Book
+
+def main(request):
+    books = Book.objects.order_by('-publication_date').filter()[:5]
+    return render(request, "book_list.html", {
+        'books': books
+    });
